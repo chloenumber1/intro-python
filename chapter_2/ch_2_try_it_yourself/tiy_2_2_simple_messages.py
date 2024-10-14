305a7e2699f5d0f959f2203eebf2b98f66182ce9
chloenumber1 <emailacctspare1@gmail.com>
2024-10-14 13:43:43 -0400

    Added tiy_2_2_simple_messages.py

---
+++ chapter_2/ch_2_try_it_yourself/tiy_2_2_simple_messages.py
@@ -0,0 +1,8 @@
+# 2-2. Simple Messages: Assign a message to a variable, and print that 
+# message. Then change the value of the variable to a new message, 
+# and print the new message.
+
+message = "This is the first message assigned to a variable named message."
+print(message)
+message = "Now, I reassign the value of message to this string."
+print(message)
