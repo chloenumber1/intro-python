fe2750456ffc063e3c7e37ebde19bc54ab483543
chloenumber1 <emailacctspare1@gmail.com>
2024-10-21 23:48:26 -0400

    Changes to tiy_2_5

---
+++ chapter_2/ch_2_try_it_yourself/tiy_2_5_famous_quote.py
@@ -0,0 +1,25 @@
+# 2-5. Famous Quote: Find a quote from a famous person you admire. 
+# Print the quote and the name of its author. 
+
+# Solution 1: Print statements with some escape sequences
+
+print("Solution 1:")
+print("The Buddha once said,")
+print("\"All that we are is the result of what we have thought.")
+print("It is founded on our thoughts; \nit is made up of our thoughts.\"\n\n")
+
+# Solution 2: One giant print statement with escape sequences
+
+print("Solution 2:")
+print("The Buddha once said,\n\"All that we are is the result of what we have thought. \nIt is founded on our thoughts; \nit is made up of our thoughts.\"\n\n")
+
+# Solution 3: Print statements only, no escape sequences.
+# Done with single vs. double quotes
+
+print("Solution 3:")
+print("The Buddha once said,")
+print('"All that we are is the result of what we have thought.')
+print('It is founded in our thoughts;')
+print('it is made up of our thoughts"')
+print()
+print()
