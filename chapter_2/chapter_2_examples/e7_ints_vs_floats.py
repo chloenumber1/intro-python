46fa6088064ce92090337c6601adb03e8c0186b0
chloenumber1 <emailacctspare1@gmail.com>
2024-10-22 11:42:51 -0400

    e7_ints_vs_floats.py code added

--- chapter_2/chapter_2_examples/e7_ints_vs_floats.py
+++ chapter_2/chapter_2_examples/e7_ints_vs_floats.py
@@ -1,1 +1,25 @@
-#
+# 2.4.3 Integers and Floats
+
+# 2.4.3.1 Division
+# Integer-only division still outputs a float
+print("Integer-only division outputs a float:")
+int_only_div = 4/2
+print(f"4/2={int_only_div}\n")
+
+# 2.4.3.2 Integer and Float Operations
+# Math done with both an integer and float outputs a float
+int_float_div = 4/2.0
+print("Integer and float division outputs a float:")
+print(f"4/2.0={int_float_div}\n")
+
+int_float_mult = 4*2.0
+print("Integer and float multiplication outputs a float:")
+print(f"4*2.0={int_float_mult}\n")
+
+int_float_add = 4+2.0
+print("Integer and float addition outputs a float:")
+print(f"4+2.0={int_float_add}\n")
+
+int_float_sub = 4-2.0
+print("Integer and float subtraction outputs a float:")
+print(f"4-2.0={int_float_sub}\n")
