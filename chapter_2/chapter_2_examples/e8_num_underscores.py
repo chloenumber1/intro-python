a4a20b06aaf3e59ce0e12351f39b573f6dcec6c7
chloenumber1 <emailacctspare1@gmail.com>
2024-10-22 11:46:21 -0400

    e8_num_underscores.py code added

--- chapter_2/chapter_2_examples/e8_num_underscores.py
+++ chapter_2/chapter_2_examples/e8_num_underscores.py
@@ -1,1 +1,9 @@
-#
+# 2.4.4 Underscores in numbers
+# You can add underscores to numbers for readibility but
+# they'll still print without the underscores.
+
+num_underscores = 1_000_000
+print(f"num_underscores stored as 1_000_000 but when printed = {num_underscores}")
+
+num = 1000000
+print(f"num stored as 1000000 and when printed = {num}")
