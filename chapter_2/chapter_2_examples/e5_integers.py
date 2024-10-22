24552529e291882b695497982452613c1ffcd851
chloenumber1 <emailacctspare1@gmail.com>
2024-10-22 11:23:20 -0400

    Code added to e5_integers.py

--- chapter_2/chapter_2_examples/e5_integers.py
+++ chapter_2/chapter_2_examples/e5_integers.py
@@ -1,1 +1,33 @@
-#
+# 2.4.1 Integers
+
+# 2.4.1.1 Integer Operations
+
+# Addition
+print("Addition")
+addition = 1+1
+print(f"1+1={addition}\n")
+
+# Subtraction
+print("Subtraction")
+subtraction = 28-1
+print(f"28-1={subtraction}\n")
+
+# Multiplication
+print("Multiplication")
+multiplication = 25*4
+print(f"25*4={multiplication}\n")
+
+# Division
+print("Division")
+division = 100/4
+print(f"100/4={division}\n")
+
+# 2.4.1.2 Exponentiation 
+print("Exponentiation")
+exponentiation = 3**3
+print(f"3**3={exponentiation}\n")
+
+# 2.4.1.3 Order of Operations
+print("Order of Operations")
+oop = 2-7+3*8+1
+print(f"2-7+3*8+1={oop}\n")
