deb1ade88b79d3802d852f24d7062a6e64789595
chloenumber1 <emailacctspare1@gmail.com>
2024-10-21 23:13:13 -0400

    tiy_2_3_personal_message.py added

---
+++ chapter_2/ch_2_try_it_yourself/tiy_2_3_personal_message.py
@@ -0,0 +1,11 @@
+# 2-3. Personal Message: Use a variable to represent a person’s name, 
+# and print a message to that person. Your message should be simple, 
+# such as, “Hello Eric, would you like to learn some Python today?”
+
+# Solution 1
+person_name = "Susie"
+print(f"Hello {person_name}, I made this F-String for you.")
+
+# Solution 2
+message = f"Hello {person_name}, this time, I assigned the F_String to a variable named msg."
+print(message)
