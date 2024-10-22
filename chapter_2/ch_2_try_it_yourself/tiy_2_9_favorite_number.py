53da66aa50b3ab1a4fdcf05492887a94a9bfd340
chloenumber1 <emailacctspare1@gmail.com>
2024-10-22 12:29:03 -0400

    Added tiy_2_9_favorite_number.py

---
+++ chapter_2/ch_2_try_it_yourself/tiy_2_9_favorite_number.py
@@ -0,0 +1,10 @@
+# 2-9. Favorite Number: Use a variable to represent your favorite number. 
+# Then, using that variable, create a message that reveals your 
+# favorite number. Print that message.
+
+# Creating a variable to represent my favorite number
+favorite_number = 27
+# Creating a message variable to reveal my favorite number
+message_fav_num = f"My favorite number is {favorite_number}"
+# Printing the message revealing the number
+print(message_fav_num)
