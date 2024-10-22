f699e436680df89f3f728abe88df22abf37f5365
chloenumber1 <emailacctspare1@gmail.com>
2024-10-22 00:13:00 -0400

    tiy_2_6_famous_quote_2.py added

---
+++ chapter_2/ch_2_try_it_yourself/tiy_2_6_famous_quote_2.py
@@ -0,0 +1,8 @@
+# 2-6. Famous Quote 2: Repeat Exercise 2-5, but this time, represent the 
+# famous person’s name using a variable called famous_person. 
+# Then compose your  message and represent it with a 
+# new variable called message. Print your message.
+
+name = "Buddha"
+message = f"The {name} once said,\n\"All that we are is the result of what we have thought. \nIt is founded on our thoughts; \nit is made up of our thoughts.\"\n\n"
+print(message)
