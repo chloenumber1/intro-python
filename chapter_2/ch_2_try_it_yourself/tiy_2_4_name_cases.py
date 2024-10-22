c7664d2169b3c6d2f926aefb89d6fafbe13f44d1
chloenumber1 <emailacctspare1@gmail.com>
2024-10-21 23:17:49 -0400

    Added tiy_2_4_name_cases.py

---
+++ chapter_2/ch_2_try_it_yourself/tiy_2_4_name_cases.py
@@ -0,0 +1,14 @@
+# 2-4. Name Cases: Use a variable to represent a person’s name, 
+# and then print that person’s name in 
+# lowercase, uppercase, and title case.
+
+# lowercase
+name = "SUSIE SUE"
+print(name.lower())
+
+# UPPERCASE and reassignment
+name = "susie sue"
+print(name.upper())
+
+# Title Case
+print(name.title())
