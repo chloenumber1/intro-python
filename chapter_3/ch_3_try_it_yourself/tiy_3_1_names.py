594a013ba9f8af6ae06c10e4994e386d85d3bd9e
chloenumber1 <emailacctspare1@gmail.com>
2024-10-23 12:43:43 -0400

    tiy 3_1 added

---
+++ chapter_3/ch_3_try_it_yourself/tiy_3_1_names.py
@@ -0,0 +1,11 @@
+# 3-1. Names: Store the names of a few of your friends in a 
+# list called names. Print  each person’s name by accessing 
+# each element in the list, one at a time.
+
+# Store the names of a few of your friends in a list called names:
+names = ["Susie", "Sue", "Goblin", "Aaron"]
+# Print each element:
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
