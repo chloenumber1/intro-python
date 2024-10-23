b1604462099844952187504eed323206ecf869bf
chloenumber1 <emailacctspare1@gmail.com>
2024-10-23 12:47:11 -0400

    tiy 3_2 added

---
+++ chapter_3/ch_3_try_it_yourself/tiy_3_2_greetings.py
@@ -0,0 +1,12 @@
+# 3-2. Greetings: Start with the list you used in Exercise 3-1, 
+# but instead of just printing each person’s name, 
+# print a message to them. The text of each message should be the same, 
+# but each message should be personalized with the person’s name.
+
+# Start with the list from 3-1:
+names = ["Susie", "Sue", "Goblin", "Aaron"]
+# Print each message with the elements:
+print(f"Hello there, {names[0]}")
+print(f"Hello there, {names[1]}")
+print(f"Hello there, {names[2]}")
+print(f"Hello there, {names[3]}")
