a3052ee556f75916b5fa856e01762d2ca9faf0f7
chloenumber1 <emailacctspare1@gmail.com>
2024-12-08 23:15:39 -0500

    tiy_3_9_dinner_guests.py completed

--- chapter_3/ch_3_try_it_yourself/tiy_3_9_dinner_guests.py
+++ chapter_3/ch_3_try_it_yourself/tiy_3_9_dinner_guests.py
@@ -1,3 +1,19 @@
 # 3-9. Dinner Guests: Working with one of the programs from Exercises 3-4 
 # through 3-7 (page 42), use len() to print a message indicating the number 
 # of people you are inviting to dinner.
+
+# Making a list of invites
+
+invite = ["Mom", "William Shakespeare", "Jesus", "Moo Deng", "Pablo Picasso"]
+
+# Storing the number of guests:
+num_guests = len(invite)
+
+# Printing invites to each one using a formatted string with their element index
+
+print(f"Hi {invite[0]}, I would love to have you at my dinner party!")
+print(f"Hi {invite[1]}, I would love to have you at my dinner party!")
+print(f"Hi {invite[2]}, I would love to have you at my dinner party!")
+print(f"Hi {invite[3]}, I would love to have you at my dinner party!")
+print(f"Hi {invite[4]}, I would love to have you at my dinner party!")
+print(f"This means, we will have {num_guests} attendees!")
