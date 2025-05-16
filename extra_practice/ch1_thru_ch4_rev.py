510c1e84aefdbc941eee29a1ea781d1c228a45e7
chloenumber1 <emailacctspare1@gmail.com>
2025-05-15 20:14:17 -0400

    Editing ch.1-4 review

--- extra_practice/ch1_thru_ch4_rev.py
+++ extra_practice/ch1_thru_ch4_rev.py
@@ -1,2 +1,7 @@
 # This is a supplemental review of everything I've covered in 
 # Ch.1 through Ch.4.
+
+# Ch.1 was an intro, so I won't be doing review questions 
+# for that chapter.
+
+# Ch.2 covered variables, Strings, numbers, and comments.
