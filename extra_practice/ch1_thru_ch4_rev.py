d41f138da8564a53b62f610c6538ad8dfb4cd845
chloenumber1 <emailacctspare1@gmail.com>
2025-05-15 20:23:34 -0400

    Added ch.2 questions to ch.1-4 review

--- extra_practice/ch1_thru_ch4_rev.py
+++ extra_practice/ch1_thru_ch4_rev.py
@@ -5,3 +5,36 @@
 # for that chapter.
 
 # Ch.2 covered variables, Strings, numbers, and comments.
+
+# 2-1. Simple Message: Assign a message to a variable, and then print that 
+# message.
+
+# 2-2. Simple Messages: Assign a message to a variable, and print that 
+# message. Then change the value of the variable to a new message, 
+# and print the new message.
+
+# 2-3. Personal Message: Use a variable to represent a person’s name, 
+# and print a message to that person. Your message should be simple, 
+# such as, “Hello Eric, would you like to learn some Python today?”
+
+# 2-4. Name Cases: Use a variable to represent a person’s name, and then 
+# print that person’s name in lowercase, uppercase, and title case.
+
+# 2-5. Famous Quote: Find a quote from a famous person you admire. Print 
+# the quote and the name of its author. Your output should look something 
+# like the following, including the quotation marks:
+# Albert Einstein once said, 
+#     “A person who never made a 
+#     mistake never tried anything new.”
+
+# 2-6. Famous Quote 2: Repeat Exercise 2-5, but this time, represent the 
+# famous person’s name using a variable called famous_person. 
+# Then compose your  message and represent it with a new variable called 
+# message. Print your message.
+
+# 2-7. Stripping Names: Use a variable to represent a person’s name, and 
+# include some whitespace characters at the beginning and end of the name. 
+# Make sure you use each character combination, "\t" and "\n", at least once.
+# Print the name once, so the whitespace around the name is displayed. 
+# Then print the name using each of the three stripping functions, lstrip(), 
+# rstrip(), and strip()
