74ba07c816732e68df8e821ea2a0aa98860ba51e
chloenumber1 <emailacctspare1@gmail.com>
2025-05-17 14:30:07 -0400

    Ch.1-4 Review edits to ch.3 section

--- extra_practice/ch1_thru_ch4_rev.py
+++ extra_practice/ch1_thru_ch4_rev.py
@@ -126,20 +126,48 @@ print("----------\n")
 # names. Print each person’s name by accessing each element in the list, 
 # one at a time.
 
+print("3-1 Names List Question:\n")
+names_list3_1 = ["friend1", "friend2", "friend3", "friend4", "friend5"]
+print(names_list3_1[0])
+print(names_list3_1[1])
+print(names_list3_1[2])
+print(names_list3_1[3])
+print(names_list3_1[4])
+print("----------\n")
+
 # 3-2. Greetings: Start with the list you used in Exercise 3-1, but instead 
 # of just printing each person’s name, print a message to them. The text 
 # of each message should be the same, but each message should be personalized 
 # with the person’s name.
+print("3-2 Greetings Question:\n")
+print(f"Hi there, {names_list3_1[0].title()}")
+print(f"Hi there, {names_list3_1[1].title()}")
+print(f"Hi there, {names_list3_1[2].title()}")
+print(f"Hi there, {names_list3_1[3].title()}")
+print(f"Hi there, {names_list3_1[4].title()}")
+print("----------\n")
 
 # 3-3. Your Own List: Think of your favorite mode of transportation, such 
 # as a motorcycle or a car, and make a list that stores several examples. 
 # Use your list to print a series of statements about these items, such 
 # as “I would like to own a Honda motorcycle.”
 
+print("3-3 Your Own List Question:\n")
+transport_list3_3 = ["walking", "car", "plane", "cruise ship", "ferry"]
+print(f"Sometimes, I don't want to drive, I'd rather be {transport_list3_3[0]}.")
+print(f"I usually have to drive a {transport_list3_3[1]} to get anywhere.")
+print(f"I typically only take a {transport_list3_3[2]} for vacation, which isn't often.")
+print(f"This year, I got to spend 4 days on a {transport_list3_3[3]}.")
+print(f"I've only taken a {transport_list3_3[4]} once, but I liked it.")
+print("----------\n")
+
 # 3-4. Guest List: If you could invite anyone, living or deceased, 
 # to dinner, who would you invite? Make a list that includes at least three
 # people you’d like to invite to dinner. Then use your list to print a 
 # message to each person, inviting them to dinner.
+
+print("3-4 Guest List Question:\n")
+print("----------\n")
 
 # 3-5. Changing Guest List: You just heard that one of your guests can’t 
 # make the dinner, so you need to send out a new set of invitations. You’ll
@@ -151,6 +179,15 @@ print("----------\n")
 # 3-5.3 Print a second set of invitation messages, one for each person 
 # who is still in your list.
 
+print("3-5.1 Changing Guest List Question:\n")
+print("----------\n")
+
+print("3-5.2 Changing Guest List Question:\n")
+print("----------\n")
+
+print("3-5.3 Names Changing Guest Question:\n")
+print("----------\n")
+
 # 3-6. More Guests: You just found a bigger dinner table, so now more 
 # space is available. Think of three more guests to invite to dinner.
 # 3-6.1 Start with your program from Exercise 3-4 or Exercise 3-5. 
@@ -160,6 +197,21 @@ print("----------\n")
 # 3-6.3 Use insert() to add one new guest to the middle of your list.
 # 3-6.4 Use append() to add one new guest to the end of your list.
 # 3-6.5 Print a new set of invitation messages, one for each person in your list.
+
+print("3-6.1 More Guests Question:\n")
+print("----------\n")
+
+print("3-6.2 More Guests Question:\n")
+print("----------\n")
+
+print("3-6.3 More Guests Question:\n")
+print("----------\n")
+
+print("3-6.4 More Guests Question:\n")
+print("----------\n")
+
+print("3-6.5 More Guests Question:\n")
+print("----------\n")
 
 # 3-7. Shrinking Guest List: You just found out that your new dinner table
 # won’t arrive in time for the dinner, and you have space for only two guests.
@@ -174,6 +226,18 @@ print("----------\n")
 # 3-7.4 Use del to remove the last two names from your list, so you have an empty 
 # list. Print your list to make sure you actually have an empty list at the end 
 # of your program.
+
+print("3-7.1 Shrinking Guest List Question:\n")
+print("----------\n")
+
+print("3-7.2 Shrinking Guest List Question:\n")
+print("----------\n")
+
+print("3-7.3 Shrinking Guest List Question:\n")
+print("----------\n")
+
+print("3-7.4 Shrinking Guest List Question:\n")
+print("----------\n")
 
 # 3-8. Seeing the World: Think of at least five places in the world you’d like to 
 # visit.
@@ -196,16 +260,55 @@ print("----------\n")
 # 3-8.10 Use sort() to change your list so it’s stored in reverse 
 # alphabetical order. Print the list to show that its order has changed.
 
+print("3-8.1 Seeing The World Question:\n")
+print("----------\n")
+
+print("3-8.2 Seeing The World Question:\n")
+print("----------\n")
+
+print("3-8.3 Seeing The World Question:\n")
+print("----------\n")
+
+print("3-8.4 Seeing The World Question:\n")
+print("----------\n")
+
+print("3-8.5 Seeing The World Question:\n")
+print("----------\n")
+
+print("3-8.6 Seeing The World Question:\n")
+print("----------\n")
+
+print("3-8.7 Seeing The World Question:\n")
+print("----------\n")
+
+print("3-8.8 Seeing The World Question:\n")
+print("----------\n")
+
+print("3-8.9 Seeing The World Question:\n")
+print("----------\n")
+
+print("3-8.10 Seeing The World Question:\n")
+print("----------\n")
+
 # 3-9. Dinner Guests: Working with one of the programs from Exercises 3-4 
 # through 3-7 (page 42), use len() to print a message indicating the number 
 # of people you are inviting to dinner.
+
+print("3-9 Dinner Guests Question:\n")
+print("----------\n")
 
 # 3-10. Every Function: Think of something you could store in a list. For example, 
 # you could make a list of mountains, rivers, countries, cities, languages, or 
 # anything else you’d like. Write a program that creates a list containing these 
 # items and then uses each function introduced in this chapter at least once.
 
+print("3-10 Every Function Question:\n")
+print("----------\n")
+
 # 3-11. Intentional Error: If you haven’t received an index error in one of your 
 # programs yet, try to make one happen. Change an index in one of your 
 # programs to produce an index error. Make sure you correct the error before 
 # closing the program.
+
+print("3-11 Intentional Error Question:\n")
+print("----------\n")
