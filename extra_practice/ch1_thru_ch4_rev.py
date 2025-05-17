bce4737e00b77379288933c956a7adbe8dca6252
chloenumber1 <emailacctspare1@gmail.com>
2025-05-17 14:07:02 -0400

    Ch.1-4 review Ch.3 questions added

--- extra_practice/ch1_thru_ch4_rev.py
+++ extra_practice/ch1_thru_ch4_rev.py
@@ -115,3 +115,97 @@ print(f"This is my favorite number: {fav_num}")
 print("----------\n")
 
 # 2-10 was about adding comments, which are throughout the file.
+
+# Ch.3 was an intro to lists.
+# It covered:
+# 3.1 What is a list
+# 3.2 Changing, adding, and removing elements
+# 3.3 Organizing a list
+
+# 3-1. Names: Store the names of a few of your friends in a list called 
+# names. Print each person’s name by accessing each element in the list, 
+# one at a time.
+
+# 3-2. Greetings: Start with the list you used in Exercise 3-1, but instead 
+# of just printing each person’s name, print a message to them. The text 
+# of each message should be the same, but each message should be personalized 
+# with the person’s name.
+
+# 3-3. Your Own List: Think of your favorite mode of transportation, such 
+# as a motorcycle or a car, and make a list that stores several examples. 
+# Use your list to print a series of statements about these items, such 
+# as “I would like to own a Honda motorcycle.”
+
+# 3-4. Guest List: If you could invite anyone, living or deceased, 
+# to dinner, who would you invite? Make a list that includes at least three
+# people you’d like to invite to dinner. Then use your list to print a 
+# message to each person, inviting them to dinner.
+
+# 3-5. Changing Guest List: You just heard that one of your guests can’t 
+# make the dinner, so you need to send out a new set of invitations. You’ll
+# have to think of someone else to invite.
+# 3-5.1 Start with your program from Exercise 3-4. Add a print() call at 
+# the end of your program stating the name of the guest who can’t make it.
+# 3-5.2 Modify your list, replacing the name of the guest who can’t make 
+# it with the name of the new person you are inviting.
+# 3-5.3 Print a second set of invitation messages, one for each person 
+# who is still in your list.
+
+# 3-6. More Guests: You just found a bigger dinner table, so now more 
+# space is available. Think of three more guests to invite to dinner.
+# 3-6.1 Start with your program from Exercise 3-4 or Exercise 3-5. 
+# Add a print() call to the end of your program informing people that you 
+# found a bigger dinner table.
+# 3-6.2 Use insert() to add one new guest to the beginning of your list.
+# 3-6.3 Use insert() to add one new guest to the middle of your list.
+# 3-6.4 Use append() to add one new guest to the end of your list.
+# 3-6.5 Print a new set of invitation messages, one for each person in your list.
+
+# 3-7. Shrinking Guest List: You just found out that your new dinner table
+# won’t arrive in time for the dinner, and you have space for only two guests.
+# 3-7.1 Start with your program from Exercise 3-6. Add a new line that prints
+# a message saying that you can invite only two people for dinner.
+# 3-7.2 Use pop() to remove guests from your list one at a time until only 
+# two names remain in your list. Each time you pop a name from your list, 
+# print a message to that person letting them know you’re sorry you can’t 
+# invite them to dinner.
+# 3-7.3 Print a message to each of the two people still on your list, letting 
+# them know they’re still invited.
+# 3-7.4 Use del to remove the last two names from your list, so you have an empty 
+# list. Print your list to make sure you actually have an empty list at the end 
+# of your program.
+
+# 3-8. Seeing the World: Think of at least five places in the world you’d like to 
+# visit.
+# 3-8.1 Store the locations in a list. Make sure the list is not in alphabetical 
+# order.
+# 3-8.2 Print your list in its original order. Don’t worry about printing the 
+# list neatly, just print it as a raw Python list.
+# 3-8.3 Use sorted() to print your list in alphabetical order without modifying the 
+# actual list.
+# 3-8.4 Show that your list is still in its original order by printing it.
+# 3-8.5 Use sorted() to print your list in reverse alphabetical order without 
+# changing the order of the original list.
+# 3-8.6 Show that your list is still in its original order by printing it again.
+# 3-8.7 Use reverse() to change the order of your list. Print the list to show 
+# that its order has changed.
+# 3-8.8 Use reverse() to change the order of your list again. Print the list to 
+# show it’s back to its original order.
+# 3-8.9 Use sort() to change your list so it’s stored in alphabetical order. 
+# Print the list to show that its order has been changed.
+# 3-8.10 Use sort() to change your list so it’s stored in reverse 
+# alphabetical order. Print the list to show that its order has changed.
+
+# 3-9. Dinner Guests: Working with one of the programs from Exercises 3-4 
+# through 3-7 (page 42), use len() to print a message indicating the number 
+# of people you are inviting to dinner.
+
+# 3-10. Every Function: Think of something you could store in a list. For example, 
+# you could make a list of mountains, rivers, countries, cities, languages, or 
+# anything else you’d like. Write a program that creates a list containing these 
+# items and then uses each function introduced in this chapter at least once.
+
+# 3-11. Intentional Error: If you haven’t received an index error in one of your 
+# programs yet, try to make one happen. Change an index in one of your 
+# programs to produce an index error. Make sure you correct the error before 
+# closing the program.
