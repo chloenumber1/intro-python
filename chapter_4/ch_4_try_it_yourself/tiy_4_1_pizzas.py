5945a3d15c7c9a38c3b1e082af157acbbb3ed9d0
chloenumber1 <emailacctspare1@gmail.com>
2025-05-19 19:35:57 -0400

    Edits to ch.4

--- chapter_4/ch_4_try_it_yourself/tiy_4_1_pizzas.py
+++ chapter_4/ch_4_try_it_yourself/tiy_4_1_pizzas.py
@@ -1,11 +1,31 @@
-# 4-1. Pizzas: Think of at least three kinds of your favorite pizza. 
+# 4-1.1 Pizzas: Think of at least three kinds of your favorite pizza. 
 # Store these pizza names in a list, and then use a for loop to print 
 # the name of each pizza.
-# 4-1.1	Modify your for loop to print a sentence using the name of the 
+# 4-1.2	Modify your for loop to print a sentence using the name of the 
 # pizza instead of printing just the name of the pizza. For each pizza you 
 # should have one line of output containing a simple statement like I like 
 # pepperoni pizza.
-# 4-1.2 Add a line at the end of your program, outside the for loop, 
+# 4-1.3 Add a line at the end of your program, outside the for loop, 
 # that states how much you like pizza. The output should consist of three 
 # or more lines about the kinds of pizza you like and then an additional sentence, 
 # such as I really love pizza!
+
+print("4-1.1 Printing the pizza list")
+pizzas_4_1_1 = ["gyro pizza", "pan pizza", "Sicilian pizza", "pizza bagel", "NY pizza"]
+for pizza in pizzas_4_1_1:
+    print(pizza)
+print("-----End of 4-1.1-----\n")
+
+print("4-1.2 Printing a statement in the pizza loop")
+pizzas_4_1_2 = ["alfredo pizza", "pan pizza", "stuffed crust pizza", "pizza bagel", "NY pizza"]
+for pizza in pizzas_4_1_2:
+    print(f"One of my favorite kinds of pizza is {pizza.upper()}!!!")
+print("-----End of 4-1.2-----\n")
+
+print("4-1.3 Printing outside of the pizza loop")
+pizzas_4_1_3 = ["alfredo pizza", "pan pizza", "stuffed crust pizza", "pizza bagel", "buffalo chicken pizza"]
+for pizza in pizzas_4_1_3:
+    print(f"One of my favorite kinds of pizza is {pizza.upper()}!!!")
+print("Although I like those pizzas...")
+print("I think my favorite pizza is actually Costco pizza.")
+print("-----End of 4-1.3-----\n")
--- chapter_4/ch_4_try_it_yourself/tiy_4_2_animals.py
+++ chapter_4/ch_4_try_it_yourself/tiy_4_2_animals.py
@@ -1,8 +1,27 @@
-# 4-2. Animals: Think of at least three different animals that 
+# 4-2.1. Animals: Think of at least three different animals that 
 # have a common characteristic. Store the names of these animals in
 # a list, and then use a for loop to print out the name of each animal.
-# 4-2.1 Modify your program to print a statement about each animal, such as 
+# 4-2.2 Modify your program to print a statement about each animal, such as 
 # A dog would make a great pet.
-# 4-2.2 Add a line at the end of your program stating what these animals have 
+# 4-2.3 Add a line at the end of your program stating what these animals have 
 # in common. You could print a sentence such as Any of these animals would 
 # make a great pet!
+
+print("4-2.1 Listing the Animals")
+animals_4_2_1 = ["shih tzu", "bunny", "red panda", "himalayan cat", "chinchilla"]
+for animal in animals_4_2_1:
+    print(animal)
+print("-----End of 4-2.1-----\n")   
+
+print("4-2.2 Printing a Statement in the Animal loop")
+animals_4_2_2 = ["shih tzu", "bunny", "red panda", "mini horse", "chinchilla"]
+for animal in animals_4_2_2:
+    print(f"{animal} is so cute and fun.")
+print("-----End of 4-2.2-----\n")
+
+print("4-2.3 Printing Outside of the Animal loop")
+animals_4_2_3 = ["shih tzu", "bunny", "red panda", "otter", "chinchilla"]
+for animal in animals_4_2_3:
+    print(f"{animal} is so cute and fun, I want to pet it.")
+print("All of these animals are super cute and fuzzy")
+print("-----End of 4-2.2-----\n")
